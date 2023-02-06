db9407f3f0e620ca26bc998b33d17a3ccc293cca
github-private <pontus.linde@me.com>
2023-02-06 14:06:49 +0100

    Created first version for loginScreen

---
+++ frontend/mobile_app/assets/colors/colors.js
@@ -0,0 +1,16 @@
+const colors = {
+    darkGrey: '#9F9F9F',
+    lightGrey: '#D1D1D1',
+    white: '#FFFFFF',
+    black: '#000000',
+    offBlack: '#1E1E1E',
+    darkBlue: '#192CDD',
+    blue: '#2768E7',
+    lightBlue: '#4ED4FF',
+    red: '#FF4E4E',
+    brightGreen: '#41D933'
+
+};
+
+export default colors;
+
---
+++ frontend/mobile_app/assets/data/newsData.js
@@ -0,0 +1,25 @@
+const newsData = [
+    {
+        id: 'news-1',
+        title: 'Beslut: Vissa straff ska kunna avtjänas i annat EU-land',
+        image: require('../images/EU.png'),
+        description: 'Lorem ipsum jjvbjfknnkfldjkf vnojidfkfjd  vkfjfndlmvdf',
+        date: '01/01-2023',
+        topic1: 'EU',
+        topic2: 'Brott',
+        topic3: 'Straff'
+    },
+    {
+        id: 'news-2',
+        title: 'Karlsson på taket blir fängslad',
+        image: require('../images/EU.png'),
+        description: 'Lorem ipsum jjvbjfknnkfldjkf vnojidfkfjd  vkfjfndlmvdf',
+        date: '02/01-2023',
+        topic1: 'Polis',
+        topic2: 'Kändisar',
+        topic3: 'Straff'
+    }
+]
+
+export default newsData;
+
