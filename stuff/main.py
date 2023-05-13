e08a52367022c7bf9d5acca3b1fab855e40d1dc3
holma91 <alexanderholmberg91@gmail.com>
2023-05-13 15:41:43 +0200

    integrate openai

--- stuff/main.py
+++ stuff/main.py
@@ -1,42 +1,29 @@
 import json
+import time
+import logging
 import os
 import asyncio
 import aiohttp
 import requests
-from pydantic import BaseModel
+import openai
 from dotenv import load_dotenv
 from Scraper import Scraper
 
 load_dotenv()
 
-OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
-NEWSAPI_API_KEY = os.environ.get('NEWSAPI_API_KEY')
-MEDIASTACK_API_KEY = os.environ.get('MEDIASTACK_API_KEY')
+OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
+NEWSAPI_API_KEY = os.environ.get("NEWSAPI_API_KEY")
+MEDIASTACK_API_KEY = os.environ.get("MEDIASTACK_API_KEY")
 
-MEDIASTACK_BASE = 'http://api.mediastack.com/v1'
-NEWSAPI_BASE = 'https://newsapi.org/v2'
-
-test_urls = [
-    "https://www.dn.se/sverige/hogg-ihjal-tva-angripare-tingsratten-friar-22-aring-for-dramat-i-ulricehamn/",
-    "https://www.dn.se/sverige/drapatalade-22-aringen-i-ulricehamn-slappt-ur-haktet/",
-    "https://www.dn.se/sverige/stjarnkrogaren-om-restauranger-som-koper-stulet-kott-snedvrider-konkurrensen/",
-]
+MEDIASTACK_BASE = "http://api.mediastack.com/v1"
+NEWSAPI_BASE = "https://newsapi.org/v2"
 
 
-class Article(BaseModel):
-    title: str
-    intro: str
-    body: str
-
-
-async def get_article(doc):
-    article_data = json.loads(
-        doc.xpath('//script[@type="application/ld+json"]')[0].text, strict=False
-    )
-
-    title = doc.xpath('//*[@class="article__title"]/text()')[0]
-    intro = doc.xpath('//*[@class="article__content"]/div[1]/p/text()')
-    body = doc.xpath('//*[@class="article__body"]/p/text()')
+def get_article(doc):
+    # this function assumes that the source is svt
+    title = doc.xpath('//*[@class="nyh_article__heading"]/text()')[0]
+    intro = doc.xpath('//*[@class="nyh_article__lead"]/p/text()')
+    body = doc.xpath('//*[@class="nyh_article-body"]/p/text()')
     intro_processed = ""
     for p in intro:
         intro_processed += p
@@ -44,34 +31,76 @@ async def get_article(doc):
     for p in body:
         body_processed += p
 
-    return Article(title=title, intro=intro_processed, body=body_processed)
+    return {
+        "title": title,
+        "intro": intro_processed,
+        "body": body_processed,
+    }
 
 
 def get_metadata():
     """gets metadata from selected sources"""
-    sources = ['di.se', 'DN.se', 'svd', ]
+
+    sources = ["DN.se"]  # , 'di.se', 'svd', ]
     countries = "se"
-    res = requests.get(f"{MEDIASTACK_BASE}/news?countries={countries}&access_key={MEDIASTACK_API_KEY}")
-    articles =  res.json()
-    return list(filter(lambda article: article['source'] in sources, articles['data']))
+    res = requests.get(
+        f"{MEDIASTACK_BASE}/news?countries={countries}&access_key={MEDIASTACK_API_KEY}&limit=100"
+    )
+    articles = res.json()
+
+    return list(filter(lambda article: article["source"] in sources, articles["data"]))
+
+
+def get_svt_metadata():
+    with open("./data/svt.json", "r") as json_file:
+        data = json.load(json_file)
+    return data
+
+
+def summarize(articles):
+    openai.api_key = OPENAI_API_KEY
+    for article in articles:
+        response = openai.Completion.create(
+            model="text-davinci-003",
+            prompt=f"{article['title']}\n{article['intro']}\n{article['body']}\n\nTl;dr",
+            temperature=0.7,
+            max_tokens=250,
+            top_p=1.0,
+            frequency_penalty=0.0,
+            presence_penalty=1,
+        )
+        article["summary"] = response["choices"][0]["text"]
+        time.sleep(1)  # avoid rate limit
+
+    return articles
 
 
 async def main():
-    # continously get urls here
-    metadata = get_metadata()
+    logging.basicConfig(
+        filename="./logs/scraper.log",
+        level=logging.INFO,
+        format="%(asctime)s - %(levelname)s - %(message)s",
+    )
+
+    all_metadata = get_svt_metadata()
+
     articles = []
-    async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(limit=20)) as session:
+    async with aiohttp.ClientSession(
+        connector=aiohttp.TCPConnector(limit=20)
+    ) as session:
         scraper = Scraper(session=session)
-        for article in metadata:
-            # adjust scraper depending on source
-            doc = await scraper.get_html(article['url'])
-            article = await get_article(doc)
-            articles.append(article)
+        for metadata in all_metadata:
+            try:
+                doc = await scraper.get_html(metadata["url"])
+                article = get_article(doc)
+                articles.append(article)
+            except Exception as e:
+                print(f"Error getting article at {metadata['url']}: {e}")
+                logging.error(f"Error getting article at {metadata['url']}: {e}")
 
-    articles = [article.json() for article in articles]
-    print(metadata)
+    articles = summarize(articles)
 
-    # we have all articles, time to summarize them
+    print(json.dumps(articles))
 
     # we have all summarized articles, insert to db
 
