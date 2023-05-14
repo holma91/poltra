4cddcee622c8ee2f20b1117f11be56471e512e2d
holma91 <alexanderholmberg91@gmail.com>
2023-05-14 13:33:04 +0200

    remove all endpoints except /article

--- server/main.py
+++ server/main.py
@@ -1,13 +1,11 @@
-from typing import Union, Dict
 import json
 from fastapi import FastAPI
-from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
 origins = [
-    "http://localhost:3000", # if the frontend runs locally on port 3000
+    "http://localhost:3000",  # if the frontend runs locally on port 3000
 ]
 
 app.add_middleware(
@@ -18,36 +16,21 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-class Article(BaseModel):
-    source: Dict[str, str]
-    author: str
-    title: str
-    description: Union[str, None] = None
-    url: str
-    urlToImage: str
-    publishedAt: str
-    content: Union[str, None] = None
-
-
-# articles is our temporary database
-with open('database.json', 'r') as database_file:
-  articles = json.load(database_file)
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
-@app.post("/articles/")
-async def post_article(article: Article):
-  articles.append(article)
-  return article
 
 @app.get("/articles/")
-async def get_news(src: Union[str, None] = None):
-  if src:
-    return list(filter(lambda article: article['source']['name'] == src, articles))
-  return articles
+async def get_news():
+    articles = []
+    with open("./db.jsonl", "r") as file:
+        for line_number, line in enumerate(file):
+            try:
+                article = json.loads(line)
+                articles.append(article)
+            except Exception as e:
+                print(f"exception at line {line_number}: {e}")
 
-@app.get("/articles/{article_id}")
-async def get_news(article_id: int):
-  return articles[article_id]
+    return articles
